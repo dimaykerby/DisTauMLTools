corrected axis and weighted average

Training/python/DisTauTag/Training_DisTauTag_SNNv1.py
@@ -18,6 +18,7 @@ from tensorflow.keras.models import Sequential, Model, load_model
 from tensorflow.keras.layers import Input, Dense, Conv2D, Dropout, AlphaDropout, Activation, BatchNormalization, Flatten, \
                                     Concatenate, PReLU, TimeDistributed, LSTM, Masking
 from tensorflow.keras.callbacks import Callback, ModelCheckpoint, CSVLogger
+from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2_as_graph
 from datetime import datetime
 
 import mlflow
@@ -83,19 +84,34 @@ class SpaceEdgeConv(tf.keras.layers.Layer):
         D = batch_distance_matrix_general(coor,coor)    # (N, P, P)
         D = tf.expand_dims(D, axis=-1)  # (N, P, P, 1)
         W = tf.math.exp(-10*D)  # (N, P, P, 1)
+
+        #coor = x[:, :, -self.n_dim:]
+        #print(f"coor_after = {coor.shape}")
+        #x = x[:, :, :-self.n_dim]
         
         a   = tf.tile(x, (1, P, 1))   # (N, P*P, n_features)
-        na   = tf.reshape(a, (N, P, P,-1))   # (N, P, P, n_features)
+        na   = tf.reshape(a, (N, P, P, x_shape[2]))   # (N, P, P, n_features)
 
         mask = tf.expand_dims(mask, axis=-1)    # (N, P, 1)
         mask_dim = tf.tile(mask, (1, P ,1))    # (N, P*P, 1)
         mask_dim = tf.reshape(mask_dim, (N, P, P,1))    # (N, P, P, 1)
 
+        diagonal = tf.ones(P) #(P)
+        diag_tensor = tf.linalg.tensor_diag(diagonal) #(P,P)
+        diag_tensor = tf.expand_dims(diag_tensor, axis=-1) #(P,P,1)
+        diag_tensor = tf.tile(diag_tensor, (int(N/P), P ,1))  #(N,P*P,1)
+        diag_tensor =  tf.reshape(diag_tensor, (N, P, P,1)) #(N,P,P,1)
+        diag_tensor = -1*diag_tensor+1
+
         s = na * W * mask_dim   # (N, P, P, n_features)
-        ss = tf.math.reduce_sum(s, axis = 1)    # (N, P, n_features)
+        ss = tf.reduce_sum(s, axis=2)
+
+        #tensorrr = tf.reduce_sum(mask_dim-diag_tensor,axis=2)
+        norm_ = tf.tile(tf.reduce_sum(mask_dim*diag_tensor, axis=2),(1,1,x_shape[2]))         
 
         # need to substruct the personal features because they were counted in the 's' summ
-        ss = ss - x     # (N, P, n_features) 
+        ss = ss - x     # (N, P, n_features)
+        ss = tf.divide(ss, norm_)
         x = tf.concat((x, ss), axis = 2)    # (N, P, n_features*2)
 
         ### Ax+b:
@@ -168,6 +184,7 @@ class SpaceParticleNet(tf.keras.Model):
         
         xx_emb = [self.embedding[i](x_cat[:,:,i]) for i in range(self.embedding_n)]
         
+        
         x = tf.concat((*xx_emb,x), axis = 2) # (N, P, n_categorical + n_pf_features)
         x0 = x
         for i in range(len(self.EdgeConv_layers)):
@@ -180,6 +197,9 @@ class SpaceParticleNet(tf.keras.Model):
                     x0 = x
             x = self.EdgeConv_bnorm_layers[i](x)
             x = self.EdgeConv_acti_layers[i](x)
+            #if(self.wiring_period>0):
+             #   if(i % self.wiring_period == 0 and i > 0):
+              #      x0 = x            
             if(self.dropout_rate > 0):
                 x = self.EdgeConv_dropout_layers[i](x)
 
@@ -294,6 +314,31 @@ def main(cfg: DictConfig) -> None:
 
         fit_hist = run_training(model, dataloader, False, cfg.log_suffix)
 
+        #if hasattr(model, '_collected_trainable_weights'):
+         #   trainable_count = count_params(model._collected_trainable_weights)
+        #else:
+         #   trainable_count = count_params(model.trainable_weights)
+
+        #print (trainable_count)       
+        #session = tf.compat.v1.Session()
+        #graph = tf.compat.v1.get_default_graph()
+        #print(dir(graph))
+        #print(graph.get_operations())
+        #print(get_flops())
+
+        #flops = get_flops(model, batch_size=1)/int(dl_config["Setup"]["n_tau"])
+        #batch_size = 1
+        #real_model = tf.function(model).get_concrete_function(tf.TensorSpec([batch_size] + model.compute_output_shape[1:], model.compute_output_shape.dtype))
+        #frozen_func, graph_def = convert_variables_to_constants_v2_as_graph(real_model)
+
+        #run_meta = tf.compat.v1.RunMetadata()
+        #opts = tf.compat.v1.profiler.ProfileOptionBuilder.float_operation()
+        #flops = tf.compat.v1.profiler.profile(graph=frozen_func.graph,
+                                     #         run_meta=run_meta, cmd='op', options=opts)
+        #Flops = flops.total_float_ops
+        #print(f"Flops = {Flops}")    
+
+
         mlflow.log_dict(training_cfg, 'input_cfg/training_cfg.yaml')
         mlflow.log_artifact(scaling_cfg, 'input_cfg')
         mlflow.log_artifact(to_absolute_path("Training_DisTauTag_SNNv1.py"), 'input_cfg')
@@ -301,6 +346,8 @@ def main(cfg: DictConfig) -> None:
         mlflow.log_artifacts('.hydra', 'input_cfg/hydra')
         mlflow.log_artifact('Training_DisTauTag_SNNv1.log', 'input_cfg/hydra')
         mlflow.log_param('run_id', run_id)
+        #mlflow.log_param('FLOPs', flops)
+        #mlflow.log_param('trainable_params', trainable_count)
         print(f'\nTraining has finished! Corresponding MLflow experiment name (ID): {cfg.experiment_name}({run_kwargs["experiment_id"]}), and run ID: {run_id}\n')
 
 